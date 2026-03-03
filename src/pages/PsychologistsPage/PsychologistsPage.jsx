6b2615bd2439da6d333e3c18da3f91329eefb9c4
Baran Taşcı <barantasci44@gmail.com>
2026-03-03 20:50:30 +0300

    PsychologistsPage.jsx added

---
+++ src/pages/PsychologistsPage/PsychologistsPage.jsx
@@ -0,0 +1,13 @@
+import Psychologists from "../../components/Psychologists/Psychologists.jsx";
+import Navigation from "../../components/Navigation/Navigation.jsx";
+
+const PsychologistsPage = () => {
+  return (
+    <>
+      <Navigation />
+      <Psychologists />
+    </>
+  );
+};
+
+export default PsychologistsPage;
