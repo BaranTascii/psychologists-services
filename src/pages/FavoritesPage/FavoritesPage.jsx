68a1dd7224da94414ad6376e6e7d85ed7cc0239c
Baran Taşcı <barantasci44@gmail.com>
2026-03-03 20:52:50 +0300

    FavoritesPage.jsx added

---
+++ src/pages/FavoritesPage/FavoritesPage.jsx
@@ -0,0 +1,13 @@
+import Favorites from "../../components/Favorites/Favorites.jsx";
+import Navigation from "../../components/Navigation/Navigation.jsx";
+
+const FavoritesPage = () => {
+  return (
+    <>
+      <Navigation />
+      <Favorites />
+    </>
+  );
+};
+
+export default FavoritesPage;
