ad5a453a81a86cb5aff26468733ae99d621bee22
Baran Taşcı <barantasci44@gmail.com>
2026-03-06 21:36:00 +0300

    Loader updated

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -4,7 +4,7 @@ import css from "./Loader.module.css";
 export const Loader = () => {
   return (
     <div className={css.loaderWrapper}>
-      <RingLoader size={40} color="#18afaf" />
+      <RingLoader size={50} color="#54BE96" />
     </div>
   );
 };
